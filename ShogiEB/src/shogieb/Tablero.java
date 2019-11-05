2d5bcd39d481b29c76f49d3daea1ec4d4f1bf177
NelsonSosa <nelsonrsavallone@gmail.com>
2019-11-05 18:26:56 -0300

    Merge origin/master
    Conflicts:
    	src/VentanadeInicio/Controlador.java

---
+++ ShogiEB/src/shogieb/Tablero.java
@@ -0,0 +1,15 @@
+
+package shogieb;
+
+/**
+ *
+ * @author Nelson
+ */
+public abstract class Tablero {
+
+       static final char SIDE_LETTERS[] = {'A','B','C','D','E','F','G','H','I'};
+       static final int SIDE_NUMS[] = {1,2,3,4,5,6,7,8,9};
+       public static Cuadro tablero [][] = new Cuadro [9][9]; 
+        
+       
+    }
