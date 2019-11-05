9527af2b4c864d10fc148be0081b8b209e856eea
NelsonSosa <nelsonrsavallone@gmail.com>
2019-11-05 18:28:19 -0300

    Merge origin/master
    Conflicts:
    	src/VentanadeInicio/Controlador.java

---
+++ ShogiEB/src/shogieb/Cuadro.java
@@ -0,0 +1,27 @@
+
+package shogieb;
+
+public abstract class Cuadro {
+    
+    protected String simbolo;
+    public String color; //blanca, negra o null, para espacios vacios
+    public String tipo;
+    
+    public Cuadro (String tipoIn)
+    {
+    tipo = tipoIn;
+    }
+    
+    String getSimbolo ()
+    {
+    return simbolo;
+    }
+    
+    public String getTipo ()
+    {
+    return tipo;
+    }
+    
+    public abstract boolean checkMovimiento (int [] moverDesde, int[]moverHasta, String jugadorColor, boolean testRey);
+}
+
