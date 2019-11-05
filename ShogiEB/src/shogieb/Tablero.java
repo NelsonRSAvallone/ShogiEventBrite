6c4c6b1e90d97b8977f4d22bffa5a460ab947e3e
NelsonSosa <nelsonrsavallone@gmail.com>
2019-11-05 18:30:43 -0300

    Merge origin/master
    Conflicts:
    	src/VentanadeInicio/Controlador.java

--- ShogiEB/src/shogieb/Tablero.java
+++ ShogiEB/src/shogieb/Tablero.java
@@ -6,7 +6,7 @@ package shogieb;
  * @author Nelson
  */
 public abstract class Tablero {
-
+        //Esto va a estar dibujado al lado y arriba de la matriz.
        static final char SIDE_LETTERS[] = {'A','B','C','D','E','F','G','H','I'};
        static final int SIDE_NUMS[] = {1,2,3,4,5,6,7,8,9};
        public static Cuadro tablero [][] = new Cuadro [9][9]; 
