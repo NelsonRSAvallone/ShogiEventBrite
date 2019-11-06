e4772f0c0abefb619e0e36c24a6a93f6b149f79a
NelsonSosa <nelsonrsavallone@gmail.com>
2019-11-06 13:53:01 -0300

    Merge origin/master
    Conflicts:
    	src/VentanadeInicio/Controlador.java

--- ShogiEB/src/shogieb/Jugador.java
+++ ShogiEB/src/shogieb/Jugador.java
@@ -17,21 +17,40 @@ public class Jugador {
     {
         nombre = nombreIn;
         color = colorIn;
-        
     }
+        
     
-    private int convertirANum (char charIn)
+    private int convertirANum (char charIn)//si este metodo retorna un -1 la letra no es valida
+                                            // el usuario va a entrar un char pero necesita un numero para el programa
     {
     int numOut = -1;
         for (int i = 0; i < Tablero.SIDE_LETTERS.length; i++) 
         {
-            if (Tablero.SIDE_LETTERS[i]== charIn) {
+            if (Tablero.SIDE_LETTERS[i]== charIn) 
+            {
                 numOut = i;
             }
         }
         return numOut;
     }
     
+    //convierte el numero string a numero real int y se fija si es menor que 9
+    private int convertirCharNumANum(char charIn)
+    {
+    int numOut = -1;// is -1 es retornado el numero es invalido
+    int convertidoNum = Character.getNumericValue(charIn);
+    
+        for (int i: Tablero.SIDE_NUMS) 
+        {
+            if( i == convertidoNum)
+            {
+            numOut = convertidoNum;
+            }
+        }
+    
+    
+    return numOut;
+    }
     
     
 }
