0f68144146b9e5b4c1843363d72688bb367f686b
NelsonSosa <nelsonrsavallone@gmail.com>
2019-11-10 10:28:11 -0300

    Merge origin/master
    Conflicts:
    	src/VentanadeInicio/Controlador.java

--- ShogiEB/src/piezas/Peon.java
+++ ShogiEB/src/piezas/Peon.java
@@ -50,29 +50,20 @@ public Peon(String colorIn) {
 			
 		}
 			
-		if(moverHastaY == moverDesdeY + moveAdelanteUno){
+		if(moverHastaY == moverDesdeY + moveAdelanteUno ){
 			
-			//mover para tomar una pieza que es de diferente color en diagonal
-			if((moverHastaX == moverDesdeX - 1) || (moverHastaX == moverDesdeX + 1)){
-				if((aCuadro.getTipo() != "blank") && (aCuadro.getColor() != jugadorColor)){
-					return true; 
-				}
-			}	
-			
-                        //movimiento hacia adelante 1 a un espacio vacio
-			else if((moverHastaX == moverDesdeX) && (aCuadro.getTipo() == "blank")){ 
-				return true;
-			}
+                    if(moverHastaX == moverDesdeX + moveAdelanteUno) {
+                        return false;
+                    }
+                        return true;
 		}
-		//move forward 2 straight and is to blank space
-		//else if((moverHastaY == moverDesdeY) && (moverHastaX == moverDesdeX) && (aCuadro.getTipo() == "blank")){ 
-		//	if(moverDesdeY == pawnRowOnPlySide){ //if pawn moves from the starting row
-		//		return true;
-		//	}
-		//}
-		
-		return false; //only get here if other possiblities fail
+		return false; //solo llega hasta aqui si todo lo demas falla.
 	}	
+}
+
+			
+			
+		
+		
 
     
-}
